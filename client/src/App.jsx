7ccd3a63a977865a1d50bc95f7da2953c350b9d8
Utkarsh Awasthi <utkarsh.awasthi.961@gmail.com>
2024-04-26 00:11:50 +0530

    creted CustomerPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
 import MenuBar from "./components/MenuBar/MenuBar"
 import Main from "./components/Main/Main"
+import CustomerPage from "./pages/CustomerPage"
 
 function App() {
   return (
     <>
       <div id="dashboard" className="flex"> 
-        <MenuBar/>
-        <Main/>
-        {/* <TopBar/> */}
-        {/* <Customer/> */}
-        {/* <Product/> */}
+        <CustomerPage/>
       </div>
     </>
   )

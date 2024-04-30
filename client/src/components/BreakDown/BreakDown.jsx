eee148d364d090947a04dd1e2c084c6bf8540edd
Utkarsh Awasthi <utkarsh.awasthi.961@gmail.com>
2024-04-30 17:20:33 +0530

    completed breakdown page

--- client/src/components/BreakDown/BreakDown.jsx
+++ client/src/components/BreakDown/BreakDown.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import Header from "../Header/Header.jsx";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as Chartjs, Tooltip, Legend, ArcElement} from 'chart.js';
+
+Chartjs.register(Tooltip, Legend ,ArcElement);
 
 const BreakDown = () => {
+  const option = {};
+  const data = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 150, 100],
+      backgroundColor: [
+        "#4b496f",
+        "#1e1b4b",
+        "#18163c"
+      ],
+      hoverOffset: 4
+    }]
+  };
   return (
-    <div>BreakDown</div>
-  )
-}
+    <div className="h-full">
+      <Header heading="BREAKDOWN" des="Breakdown of Sales By category" />
+      <div className="w-full h-[80%] flex justify-center">
+        <Doughnut option={option} data={data}/>
+      </div>
+    </div>
+  );
+};
 
-export default BreakDown
+export default BreakDown;

5dd62bcce275eadde039a8e599328ab9536d757e
Utkarsh Awasthi <utkarsh.awasthi.961@gmail.com>
2024-05-25 09:32:07 +0530

    form

--- client/src/components/MainForm/MainForm.jsx
+++ client/src/components/MainForm/MainForm.jsx
@@ -12,7 +12,7 @@ function RegisterForm() {
     password: "",
   })
 
-  let handdleChange = (e) => {
+  const handdleChange = (e) => {
     console.log(e);
   }
 
@@ -20,12 +20,18 @@ function RegisterForm() {
 
 
   const handleNext = () => {
-    setStep(step + 1);
+    setStep(2);
   };
 
   const handleBack = () => {
-    setStep(step - 1);
+    setStep(1);
   };
+
+  const handleLogin = () => {
+    setStep(3);
+  };
+
+
 
   return (
     <div className="min-h-screen flex items-center justify-center w-full">
@@ -49,7 +55,7 @@ function RegisterForm() {
               <label htmlFor="Phone" className="block text-sm font-medium text-gray-700">Phone Number</label>
               <input type="text" id="Phone"className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
             </div>
-            
+
             <div>
               <button type="button" onClick={handleNext} className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Next</button>
             </div>

d79d02aa12c49fff8ebd260be9524c36f515b530
Utkarsh Awasthi <utkarsh.awasthi.961@gmail.com>
2024-05-27 15:13:57 +0530

    added country selector

--- client/src/components/AddTransaction/AddTransaction.jsx
+++ client/src/components/AddTransaction/AddTransaction.jsx
@@ -1,44 +1,48 @@
-import axios from "axios"
+import axios from "axios";
 import { useEffect, useState } from "react";
-import Header from "../Header/Header.jsx";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
+import SelectorInput from "./Selector";
 
 const AddTransaction = () => {
-  // const  = ["Product A", "Product B", "Product C"];
-  const [predefinedProducts,setdata]=useState([]);
-  const nevigate=useNavigate();
-  function getdata(){
-      axios.get("http://localhost:8000/client/products",{withCredentials:true})
-  .then((resoponse)=>{
-      console.log(resoponse.data.productWithStats);
-      if(resoponse.data.user===""){
+  // let countryData = Country.getAllCountries();
+  // const [country, setCountry] = useState(countryData[0]);
+  const [predefinedProducts, setdata] = useState([]);
+  const nevigate = useNavigate();
+
+  function getdata() {
+    axios
+      .get("http://localhost:8000/client/products", { withCredentials: true })
+      .then((resoponse) => {
+        // console.log(resoponse.data.productWithStats);
+        if (resoponse.data.user === "") {
           toast.error("login first");
           nevigate("/login");
         }
-      setdata(resoponse.data.productWithStats);
-  }).catch((error)=>{
-    toast.error("error while loading product");
-      console.log(error);
-  })
+        setdata(resoponse.data.productWithStats);
+      })
+      .catch((error) => {
+        toast.error("error while loading product");
+        console.log(error);
+      });
   }
-  useEffect(getdata,[]);
+  useEffect(getdata, []);
   const [transaction, setTransaction] = useState({
-    tnxId: '',
-    cost: '',
+    tnxId: "",
+    cost: "",
     products: [],
-    customerName: '',
-    customerPhone: '',
-    customerCountry: '',
-    customerEmail: '',
-    customerOccupation: '',
-    transactionDate:'',
+    customerName: "",
+    customerPhone: "",
+    customerCountry: "",
+    customerEmail: "",
+    customerOccupation: "",
+    transactionDate: "",
   });
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
-    if (type === 'checkbox') {
+    if (type === "checkbox") {
       setTransaction((prev) => ({
         ...prev,
         products: checked
@@ -48,164 +52,192 @@ const AddTransaction = () => {
     } else {
       setTransaction({ ...transaction, [name]: value });
     }
-    console.log(transaction.products);
+    // console.log(transaction.products);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(transaction);
-    axios.post("http://localhost:8000/management/addTransaction",{
-      tnxId: transaction.tnxId,
-      cost: transaction.cost,
-      products: transaction.products,
-      customerName:transaction.customerName,
-      customerPhone:transaction.customerPhone,
-      customerCountry:transaction.customerCountry,
-      customerOccupation:transaction.customerOccupation,
-      customerEmail:transaction.customerEmail,
-      date:transaction.transactionDate,
-    },{withCredentials:true})
-    .then((response)=>{
-      toast.success("Transaction added successfully");
-      setTransaction({
-        tnxId: '',
-        cost: '',
-        products: [],
-        customerName: '',
-        customerPhone: '',
-        customerCountry: '',
-        customerEmail: '',
-        customerOccupation: '',
+    axios
+      .post(
+        "http://localhost:8000/management/addTransaction",
+        {
+          tnxId: transaction.tnxId,
+          cost: transaction.cost,
+          products: transaction.products,
+          customerName: transaction.customerName,
+          customerPhone: transaction.customerPhone,
+          customerCountry: transaction.customerCountry,
+          customerOccupation: transaction.customerOccupation,
+          customerEmail: transaction.customerEmail,
+          date: transaction.transactionDate,
+        },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        toast.success("Transaction added successfully");
+        setTransaction({
+          tnxId: "",
+          cost: "",
+          products: [],
+          customerName: "",
+          customerPhone: "",
+          customerCountry: "",
+          customerEmail: "",
+          customerOccupation: "",
         });
         nevigate("/transaction");
-        }).catch((error)=>{
-          toast.error("Error adding transaction");
-          console.log(error);
-        })
+      })
+      .catch((error) => {
+        toast.error("Error adding transaction");
+        console.log(error);
+      });
     // Add your submission logic here
   };
 
   return (
-    <div className="max-w-md mx-auto rounded-xl shadow-md">
-      <h2 className="text-2xl font-bold mb-6">Add Transaction</h2>
+    <div className="w-full rounded-xl shadow-md h-full">
+      <h2 className="text-2xl font-bold ml-10 mb-5">Add Transaction</h2>
       <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-white">Transaction ID</label>
-          <input
-            type="text"
-            name="tnxId"
-            value={transaction.tnxId}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border text-black rounded-lg"
-            required
-          />
-        </div>
+        <div className="flex mb-5 ">
+          <div className="w-3/4 pl-10 mr-10">
+            {/****************************** Transition details ***********************************/}
+            <div className="mb-10">
+              <h2 className="text-xl font-semibold mb-2">Transition details</h2>
+              <div className="flex">
+                <div className="mb-4 mr-5 w-1/2">
+                  <label className="block text-white">Transaction ID</label>
+                  <input
+                    type="text"
+                    name="tnxId"
+                    value={transaction.tnxId}
+                    onChange={handleChange}
+                    className="w-full px-3 py-2 border text-black rounded-lg"
+                    required
+                  />
+                </div>
 
-        <div className="mb-4">
-          <label className="block text-white">Transaction Date</label>
-          <input
-            type="date"
-            name="transactionDate"
-            value={transaction.transactionDate}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border text-black rounded-lg"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-white">Cost</label>
-          <input
-            type="number"
-            name="cost"
-            value={transaction.cost}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border text-black
-            rounded-lg"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-white">Products</label>
-          {predefinedProducts.map((product) => (
-            <div key={product._id} className="flex items-center">
-              <input
-                type="checkbox"
-                name="products"
-                value={product._id}
-                checked={transaction.products.includes(product._id)}
-                onChange={handleChange}
-                className="mr-2 text-black"
-              />
-              <span>{product.name}</span>
+                <div className="mb-4 w-1/2">
+                  <label className="block text-white">Cost</label>
+                  <input
+                    type="number"
+                    name="cost"
+                    value={transaction.cost}
+                    onChange={handleChange}
+                    className="w-full px-3 py-2 border text-black rounded-lg"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="mb-4 w-[49%]">
+                <label className="block text-white">Transaction Date</label>
+                <input
+                  type="date"
+                  name="transactionDate"
+                  value={transaction.transactionDate}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border text-black rounded-lg"
+                  required
+                />
+              </div>
             </div>
-          ))}
-        </div>
 
-        <div className="mb-4">
-          <label className="block text-white">Customer Name</label>
-          <input
-            type="text"
-            name="customerName"
-            value={transaction.customerName}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border text-black rounded-lg"
-            required
-          />
-        </div>
+            {/****************************** coustomer details ***********************************/}
 
-        <div className="mb-4">
-          <label className="block text-white">Customer Phone</label>
-          <input
-            type="tel"
-            name="customerPhone"
-            value={transaction.customerPhone}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border text-black rounded-lg"
-            required
-          />
-        </div>
+            <div className="">
+              <h2 className="text-xl font-semibold mb-3"> Customer Details</h2>
+              <div className="flex">
+                <div className="mb-4 w-1/2 mr-5">
+                  <label className="block text-white ">Customer Name</label>
+                  <input
+                    type="text"
+                    name="customerName"
+                    value={transaction.customerName}
+                    onChange={handleChange}
+                    className="w-full px-3 py-2 border text-black rounded-lg"
+                    required
+                  />
+                </div>
+                <div className="mb-4 w-1/2">
+                  <label className="block text-white">Customer Country</label>
+                  {/* <input
+                    type="text"
+                    id="country"
+                    name="customerCountry"
+                    value={transaction.customerCountry}
+                    onChange={handleChange}
+                    className="w-full px-3 py-2 border text-black rounded-lg"
+                    required
+                  /> */}
 
-        <div className="mb-4">
-          <label className="block text-white">Customer Country</label>
-          <input
-            type="text"
-            name="customerCountry"
-            value={transaction.customerCountry}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border text-black rounded-lg"
-            required
-          />
-        </div>
+                  <SelectorInput value={transaction.customerCountry} />
+                </div>
+              </div>
 
-        <div className="mb-4">
-          <label className="block text-white">Customer Email</label>
-          <input
-            type="email"
-            name="customerEmail"
-            value={transaction.customerEmail}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border text-black rounded-lg"
-            required
-          />
-        </div>
+              <div className="flex">
+                <div className="mb-4 w-1/2 mr-5">
+                  <label className="block text-white">Customer Email</label>
+                  <input
+                    type="email"
+                    name="customerEmail"
+                    value={transaction.customerEmail}
+                    onChange={handleChange}
+                    className="w-full px-3 py-2 border text-black rounded-lg"
+                    required
+                  />
+                </div>
+                <div className="mb-4 w-1/2">
+                  <label className="block text-white">Customer Phone</label>
+                  <input
+                    type="tel"
+                    name="customerPhone"
+                    value={transaction.customerPhone}
+                    onChange={handleChange}
+                    className="w-full px-3 py-2 border text-black rounded-lg"
+                    required
+                  />
+                </div>
+              </div>
 
-        <div className="mb-4">
-          <label className="block text-white">Customer Occupation</label>
-          <input
-            type="text"
-            name="customerOccupation"
-            value={transaction.customerOccupation}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border text-black rounded-lg"
-            required
-          />
+              <div className="mb-4 w-[49%]">
+                <label className="block text-white">Customer Occupation</label>
+                <input
+                  type="text"
+                  name="customerOccupation"
+                  value={transaction.customerOccupation}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border text-black rounded-lg"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="mb-4">
+              <label className="block text-white text-2xl font-semibold mb-6 ">
+                Products
+              </label>
+              {predefinedProducts.map((product) => (
+                <div key={product._id} className="flex items-center ml-5">
+                  <input
+                    type="checkbox"
+                    name="products"
+                    value={product._id}
+                    checked={transaction.products.includes(product._id)}
+                    onChange={handleChange}
+                    className="mr-2 h-4 w-4 custom-checkbox"
+                  />
+                  <span>{product.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
 
         <button
           type="submit"
-          className="w-full bg-blue-500 text-white px-3 py-2 rounded-lg hover:bg-blue-600"
+          className="w-[72%] ml-10 bg-blue-700 text-white px-3 py-2 rounded-lg hover:bg-blue-900"
         >
           Add Transaction
         </button>
@@ -215,3 +247,8 @@ const AddTransaction = () => {
 };
 
 export default AddTransaction;
+
+// <div>
+//   <p className="text-teal-800 font-semibold">Country :</p>
+//   <Selector data={countryData} selected={country} setSelected={setCountry} />
+// </div>;
---
+++ client/src/components/AddTransaction/Selector.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+import { Country } from "country-state-city";
+
+
+const country = Country.getAllCountries();
+
+nsole.log(country);
+
+function SelectorInput() {
+  const [selectedPerson, setSelectedPerson] = useState(null);
+  const [query, setQuery] = useState("");
+
+  const filteredCountry =
+    query === ""
+      ? country
+      : country.filter((person) =>
+          person.name.toLowerCase().includes(query.toLowerCase())
+        );
+
+  return (
+    <div className="text-black">
+      <Combobox value={selectedPerson} onChange={setSelectedPerson}>
+        <div className="relative mt-1">
+          <Combobox.Input
+            className="w-full border border-gray-300 rounded-md py-2 pl-3 pr-10 shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            displayValue={(person) => (person ? person.name : "")}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+            <svg
+              className="h-5 w-5 text-gray-400"
+              viewBox="0 0 20 20"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path
+                d="M7 7l3-3 3 3m0 6l-3 3-3-3"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </Combobox.Button>
+        </div>
+        <Transition
+          as="div"
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          afterLeave={() => setQuery("")}
+        >
+          <Combobox.Options className="absolute mt-1 max-h-60 w-[29%] overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {filteredCountry.length === 0 && query !== "" ? (
+              <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                Nothing found.
+              </div>
+            ) : (
+              filteredCountry.map((person) => (
+                <Combobox.Option
+                  key={person.id}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? "bg-indigo-600 text-white" : "text-gray-900"
+                    }`
+                  }
+                  value={person}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {person.name}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-indigo-600"
+                          }`}
+                        >
+                          <svg
+                            className="h-5 w-5"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              clipRule="evenodd"
+                              d="M16.707 5.293a1 1 0 010 1.414L9 14.414l-3.707-3.707a1 1 0 10-1.414 1.414l4 4a1 1 0 001.414 0l8-8a1 1 0 00-1.414-1.414z"
+                            />
+                          </svg>
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))
+            )}
+          </Combobox.Options>
+        </Transition>
+      </Combobox>
+    </div>
+  );
+}
+
+export default SelectorInput;
